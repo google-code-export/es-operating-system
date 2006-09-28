255a5023c18d643a75bb98239b7bf81fc36d5912
okasaka <okasaka@d15c3b8f-5b43-0410-be93-4d5a55ce2954>
2006-09-28 06:20:54 +0000

    Fix clean build problems.

--- es/trunk/os/libes++/testsuite/hashtable.cpp
+++ es/trunk/os/libes++/testsuite/hashtable.cpp
@@ -11,20 +11,18 @@
  * purpose.  It is provided "as is" without express or implied warranty.
  */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <es.h>
 #include <es/hashtable.h>
 #include <es/uuid.h>
 #include <es/clsid.h>
 
-#define TEST(exp)                           \
-    (void) ((exp) ||                        \
-            (esPanic(__FILE__, __LINE__, "\nFailed test " #exp), 0))
-
 int main()
 {
     Hashtable<Guid, int> h(100);
 
-    esReport("h.size() : %d\n", h.size());
+    printf("h.size() : %d\n", h.size());
 
     int i;
     bool contains;
@@ -34,33 +32,33 @@ int main()
     h.add(CLSID_Monitor, 3);
 
     i = h.get(CLSID_Process);
-    esReport("h.get(CLSID_Process) : %d\n", i);
-    TEST(i == 1);
+    printf("h.get(CLSID_Process) : %d\n", i);
+    ASSERT(i == 1);
     i = h.get(CLSID_CacheFactory);
-    esReport("h.get(CLSID_CacheFactory) : %d\n", i);
-    TEST(i == 2);
+    printf("h.get(CLSID_CacheFactory) : %d\n", i);
+    ASSERT(i == 2);
     i = h.get(CLSID_Monitor);
-    esReport("h.get(CLSID_Monitor) : %d\n", i);
-    TEST(i == 3);
+    printf("h.get(CLSID_Monitor) : %d\n", i);
+    ASSERT(i == 3);
     try
     {
         i = h.get(CLSID_Alarm);
     }
     catch (Exception& error)
     {
-        TEST(error.getResult() == ENOENT);
-        esReport("h.get(CLSID_Alarm) : exception %d\n", error.getResult());
+        ASSERT(error.getResult() == ENOENT);
+        printf("h.get(CLSID_Alarm) : exception %d\n", error.getResult());
     }
 
     contains = h.remove(CLSID_Monitor);
-    esReport("h.remove(CLSID_Monitor) : %d\n", contains);
-    TEST(contains == true);
+    printf("h.remove(CLSID_Monitor) : %d\n", contains);
+    ASSERT(contains == true);
     contains = h.remove(CLSID_Monitor);
-    esReport("h.remove(CLSID_Monitor) : %d\n", contains);
-    TEST(contains == false);
+    printf("h.remove(CLSID_Monitor) : %d\n", contains);
+    ASSERT(contains == false);
     contains = h.contains(CLSID_Monitor);
-    esReport("h.contains(CLSID_Monitor) : %d\n", contains);
-    TEST(contains == false);
+    printf("h.contains(CLSID_Monitor) : %d\n", contains);
+    ASSERT(contains == false);
 
     try
     {
@@ -68,14 +66,14 @@ int main()
     }
     catch (Exception& error)
     {
-        TEST(error.getResult() == EEXIST);
-        esReport("h.get(CLSID_Process) : exception %d\n", error.getResult());
+        ASSERT(error.getResult() == EEXIST);
+        printf("h.get(CLSID_Process) : exception %d\n", error.getResult());
     }
 
     h.clear();
     contains = h.contains(CLSID_Process);
-    esReport("h.contains(CLSID_Process) : %d\n", contains);
-    TEST(contains == false);
+    printf("h.contains(CLSID_Process) : %d\n", contains);
+    ASSERT(contains == false);
 
-    esReport("done.\n");
+    printf("done.\n");
 }
